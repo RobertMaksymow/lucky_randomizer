860120ab3a09a2d6a6767b1e7b376ea28cab6ef8
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-01-13 11:28:56 +0000

    Generates valid random move

---
+++ src/main/java/com/thg/accelerator23/connectn/ai/lucky_randomizer/MayTheFourBeWithYou.java
@@ -0,0 +1,26 @@
+package com.thg.accelerator23.connectn.ai.lucky_randomizer;
+import com.thehutgroup.accelerator.connectn.player.Board;
+import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.Player;
+import java.util.List;
+import java.util.Random;
+
+//TODO: Once done, remember to add version to json file (line 29 of README) and JITPACKy thingy
+
+public class MayTheFourBeWithYou extends Player {
+  public MayTheFourBeWithYou(Counter counter) {
+    super(counter, MayTheFourBeWithYou.class.getName());
+  }
+
+  @Override
+  public int makeMove(Board board) {
+    CheckWhichColumnsAreEmpty checkWhichColumnsAreEmpty = new CheckWhichColumnsAreEmpty(board);
+    List<Integer> emptyColumns = checkWhichColumnsAreEmpty.fullColumnChecker();
+    int randomNumber = new Random().nextInt(0, emptyColumns.size());
+
+    System.out.println("Random number: " + randomNumber);
+    System.out.println("Available columns: " + emptyColumns);
+
+    return emptyColumns.get(randomNumber);
+  }
+}
